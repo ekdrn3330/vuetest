18f1d524d78b076652deb4f88cbf6611d8fc4549
KimJinHyeok <ekdrn3330@naver.com>
2021-08-09 01:34:32 +0900

    210809

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<template>
+
+  <Modal @closeModal="모달창열렸니 = false;" :원룸들="원룸들" :누른거="누른거" :모달창열렸니="모달창열렸니" />
+
+  <div class="menu">
+    <a href="#" v-for="i in 메뉴들" :key="i">{{ i }}</a>
+  </div>
+
+  <Discount/>
+
+  <Card @openModal="모달창열렸니 = true; 누른거 = $event;" :원룸="원룸들[a]" v-for="(i, a) in 원룸들" :key="i" />
+
+  <!-- <div v-for="(a,i) in 원룸들" :key="i">
+    <img :src="a.image" alt="room_img" class="room-img">
+    <h4 @click="모달창열렸니 = true; 누른거 = i">{{ a.title }}</h4>
+    <p>{{ a.price }}원</p>
+    <button @click="신고수[i]++">허위매물신고</button> <span>신고수 : {{ 신고수[i] }}</span>
+  </div> -->
+
+</template>
+
+<script>
+
+import data from './assets/oneroom.js';
+import Discount from './Discount.vue';
+import Modal from './Modal.vue';
+import Card from './Card.vue';
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      누른거 : 0,
+      원룸들 : data,
+      모달창열렸니 : false,
+      신고수 : [0, 0, 0],
+      메뉴들 : ['Home', 'Shop', 'About'],
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods: {
+    increase(){
+      this.신고수 += 1;
+    }
+  },
+  components: {
+    Discount,
+    Modal,
+    Card
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.discount {
+  background:#eee;
+  padding:10px;
+  margin:10px;
+  border-radius:5px;
+}
+
+.black-bg {
+  width:100%; height:100%;
+  background: rgba(0,0,0,.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width:100%; background:#fff;
+  border-radius: 8px;
+  padding:20px;
+}
+
+.room-img {
+  width: 400px;
+  margin-top: 40px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color:#fff;
+  padding: 10px;
+}
+</style>
---
+++ src/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+
+  <div class="black-bg" v-if="모달창열렸니 == true">
+    <div class="white-bg">
+      <h4>{{ 원룸들[누른거].title }}</h4>
+      <p>{{ 원룸들[누른거].content }}</p>
+      <input v-model="month">
+      <p>{{ month }}개월 선택함 : {{ 원룸들[누른거].price * month }} 원</p>
+
+      <button @click="$emit('closeModal')">닫기</button>
+      <Discount/>
+    </div>
+  </div>
+
+</template>
+
+<script>
+
+import Discount from './Discount.vue'
+
+export default {
+    name : 'Modal',
+    data(){
+        return {
+            month : 1,
+        }
+    },
+    watch : {
+        month(a){
+            if(a >= 13){
+                alert('13이상 입력하지 마셈');
+            } else if (isNaN(a) == true){
+                alert('문자입력하지마라');
+                this.month = 1;
+            }
+        },
+    },
+    props : {
+        원룸들 : Array,
+        누른거 : Number,
+        모달창열렸니 : Boolean,
+    },
+    components: {
+        Discount,
+    }
+}
+</script>
+
+<style>
+
+</style>
